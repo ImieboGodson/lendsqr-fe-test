d972dcb5e7f1aae9c86638110f84d8c1b143fe01
godson imiebo <imiebo.godson@gmail.com>
2022-11-01 21:09:05 +0100

    feat: added skeleton loaders

--- src/components/UsersStatsCard/UsersStatsCard.tsx
+++ src/components/UsersStatsCard/UsersStatsCard.tsx
@@ -1,10 +1,12 @@
+import { useAppSelector } from '../../utils/hooks';
 import './UsersStatsCard.scss';
 
 type CardData = {
-    icon: string;
-    color: string;
-    title: string;
-    value: string;
+  id: string;
+  icon: string;
+  color: string;
+  title: string;
+  value: string;
 }
 
 interface CardProps {
@@ -12,13 +14,35 @@ interface CardProps {
   }
 
 const UsersStatsCard: React.FC<CardProps> = ({ card }) => {
+
+  const { loading } = useAppSelector(state => state.users)
+
   return (
     <div className='users-stats-box'>
-        <span className='users-stats-box__icon_wrapper' style={{ background: `${card.color}` }}>
-            <img src={process.env.PUBLIC_URL + `${card.icon}`} alt='user'  className='users-stats-box__icon'/>
+        <span className={`users-stats-box__icon_wrapper ${loading ? 'skeleton' : ''}`} style={{ background: `${loading ? 'none' : card.color}` }}>
+            {
+              (loading) ?
+              ''
+              :
+              <img src={process.env.PUBLIC_URL + `${card.icon}`} alt='user'  className='users-stats-box__icon'/>
+            }
         </span>
-        <p className='users-stats-box__text'>{card.title}</p>
-        <p className='users-stats-box__value'>{card.value}</p>
+        {
+          (loading) ?
+          (
+            <>
+              <span className='users-stats-box__skeleton-loader skeleton'></span>
+              <span className='users-stats-box__skeleton-loader skeleton'></span>
+            </>
+          )
+          :
+          (
+            <>
+              <p className='users-stats-box__text'>{card.title}</p>
+              <p className='users-stats-box__value'>{card.value}</p>
+            </>
+          )
+        }
     </div>
   )
 }
--- src/db.ts
+++ src/db.ts
@@ -12,3 +12,43 @@ export const Profiles: LoginProfile[] = [
         password: "testlender"
     }
 ]
+
+
+interface UsersData {
+    id: string;
+    icon: string; 
+    color: string;
+    title: string;
+    value: string;
+}
+
+export const UsersDataArray: UsersData[] = [
+    {
+        id: '1',
+        icon: '/icons/users-icon.svg', 
+        color: 'rgba(223, 24, 255, 0.1)', 
+        title: 'users', 
+        value: '113,039'
+    },
+    {
+        id: '2',
+        icon: '/icons/active-users-icon.svg', 
+        color: 'rgba(87, 24, 255, 0.1)', 
+        title: 'active users', 
+        value: '2,345'
+    },
+    {
+        id: '3',
+        icon: '/icons/users-with-loans-icon.svg', 
+        color: 'rgba(245, 95, 68, 0.1)', 
+        title: 'users with loans', 
+        value: '12,453'
+    },
+    {
+        id: '4',
+        icon: '/icons/users-with-savings-icon.svg', 
+        color: 'rgba(255, 51, 102, 0.1)', 
+        title: 'users with savings', 
+        value: '102,453'
+    }
+]
