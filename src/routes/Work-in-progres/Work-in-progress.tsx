f1c64d0a439f82cb0caeda55e105d4677dc5e5cb
godson imiebo <imiebo.godson@gmail.com>
2022-11-29 15:07:13 +0100

    style:minor change on work-in-progress

--- src/routes/Work-in-progres/Work-in-progress.tsx
+++ src/routes/Work-in-progres/Work-in-progress.tsx
@@ -6,7 +6,7 @@ const Error = () => {
         <div className='work-in-progress__center-container'>
             <img src={process.env.PUBLIC_URL + '/images/contruction-worker.gif'} alt='icon'  className='work-in-progress__image'/>
             <div className='work-in-progress__text_wrapper'>
-              <p className='work-in-progress__text'>Huh... Work is ongoing here, check back later.</p>
+              <p className='work-in-progress__text'>Huh... Work in progress, check back later.</p>
             </div>
         </div>
     </div>
